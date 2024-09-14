1ccec33027dc166d0baba5b6ea1ee4f43b32ee74
Bowen Liu <reouct@gmail.com>
2024-09-14 08:56:16 -0600

    Update addMoves.java

--- shared/src/main/java/chess/addMoves.java
+++ shared/src/main/java/chess/addMoves.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static chess.ChessPiece.PieceType.QUEEN;
@@ -12,6 +13,29 @@ public class addMoves {
 
     public static ArrayList<ChessMove> addBishopMoves(ChessBoard board, ChessPosition myPosition){
     ArrayList<ChessMove> moves = new ArrayList<>();
+
+    int my_currentrow = myPosition.getRow();
+    int my_currentcol = myPosition.getColumn();
+
+    for (int row = my_currentrow+1, col = my_currentcol+1; row <= 8 && col <=8 ; ++row, ++col){
+        moves.add(new ChessMove(new ChessPosition(row, col),myPosition,null));
+        //moves.add(new ChessMove(new ChessPosition(x,y),myPosition,null));
+    }
+
+    //System.out.println(Arrays.toString(moves));
+
+    for (int row = my_currentrow-1, col = my_currentcol+1; row <= 8 && col <=8 ; --row, ++col ){
+        moves.add(new ChessMove(new ChessPosition(row,col),myPosition,null));
+    }
+
+
+    for (int row = my_currentrow-1, col = my_currentcol-1;row <= 8 && col <= 8; --row, --col){
+        moves.add(new ChessMove(new ChessPosition(row,col),myPosition,null));
+    }
+
+    for (int row = my_currentrow+1, col = my_currentcol-1; row <= 8 && col <=8; ++row,--col){
+        moves.add(new ChessMove(new ChessPosition(row,col),myPosition,null));
+    }
 
     return moves;
 
