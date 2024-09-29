d0a2dffe9346af386610813a2f5c1a2948a5024a
Bowen Liu <reouct@gmail.com>
2024-09-29 09:23:02 -0600

    Finish getTeamTurn

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -105,7 +105,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return turn;
     }
 
     /**
