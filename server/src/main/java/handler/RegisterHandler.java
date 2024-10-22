0495d9db334188df21bedbb974acf60cc10b3c91
Bowen Liu <reouct@gmail.com>
2024-10-22 11:17:31 -0600

    Update RegisterHandler.java

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -18,11 +18,32 @@ public class RegisterHandler {
     public Object registerUser(Request req, Response res) throws DataAccessException {
 
         UserData newUser = new Gson().fromJson(req.body(), UserData.class);
+
+        // 400 bad request
+        if (newUser.password() == null || newUser.username() == null || newUser.email() == null){
+            res.status(400);
+            return "error";
+        }
+
+        // Register New user
+//        try {
+//            registerService.registerUser(newUser);
+//            res.status(200);
+//            String authToken = registerService.createAuthToken(newUser);
+//            return new Gson().toJson(new AuthData(authToken, newUser.username()));
+//        } catch (DataAccessException e){
+//            String errorMessage = e.getMessage();
+//            if (errorMessage.equals("Error: already taken")) {
+//                res.status(403); // 403 already taken
+//            } else {
+//                res.status(500);
+//            }
+//            return "Error";
+//        }
         registerService.registerUser(newUser);
         res.status(200);
         String authToken = registerService.createAuthToken(newUser);
         return new Gson().toJson(new AuthData(authToken, newUser.username()));
-        //return "Success";
 
     }
 }
