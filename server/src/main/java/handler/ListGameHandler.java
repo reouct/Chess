971b1970a7b30ef3a763c7652c1f2e53449992f8
Bowen Liu <97483739+reouct@users.noreply.github.com>
2024-10-22 19:58:51 -0600

    Update ListGameHandler

--- server/src/main/java/handler/ListGameHandler.java
+++ server/src/main/java/handler/ListGameHandler.java
@@ -1,14 +1,23 @@
 package handler;
 
+import com.google.gson.Gson;
+import model.GameData;
 import service.ListGamesService;
 import spark.Request;
 import spark.Response;
 
 public class ListGameHandler {
+    private final ListGamesService listGamesService;
+
     public ListGameHandler(ListGamesService listGamesService) {
+        this.listGamesService = listGamesService;
     }
 
+
     public Object list(Request req, Response res) {
-        return "";
+        String data = req.headers("authorization");
+        res.status(200);
+        GameData gameData = listGamesService.listGame(data)
+                
     }
 }
