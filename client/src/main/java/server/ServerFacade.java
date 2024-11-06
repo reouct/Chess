2fdd985e97d324e4e3aa297cf86828d05b278a8e
Bowen Liu <97483739+reouct@users.noreply.github.com>
2024-11-06 14:21:39 -0700

    Pass Sample test

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,79 @@
+package server;
+
+import com.google.gson.Gson;
+import model.Result;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class ServerFacade {
+    private String authToken;
+    private String username;
+    private String urlString;
+
+    public ServerFacade(int port) {
+        this.urlString = "http://localhost:" + port;
+    }
+
+    private boolean doesOutput(String type) {
+        return type.equals("POST") || type.equals("PUT");
+    }
+
+    public <T> T makeRequest(String type, String urlString, Object request, String authToken,
+                             Class<T> responseClass) throws IOException {
+        try {
+            URL url = new URI(urlString).toURL();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setReadTimeout(5000);
+            connection.setRequestMethod(type);
+            connection.setDoOutput(doesOutput(type));
+
+            if (authToken != null) {
+                connection.addRequestProperty("Authorization", authToken);
+            }
+
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.connect();
+
+            if(doesOutput(type)) {
+                try (OutputStream requestBody = connection.getOutputStream();) {
+                    String json =  new Gson().toJson(request);
+                    requestBody.write(json.getBytes());
+                    requestBody.flush();
+                }
+            }
+
+            InputStream responseBody;
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                responseBody = connection.getInputStream();
+            }
+            else {
+                responseBody = connection.getErrorStream();
+            }
+            InputStreamReader reader = new InputStreamReader(responseBody);
+
+            return new Gson().fromJson(reader, responseClass);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public Result clear() {
+        try {
+            return makeRequest("DELETE", urlString + "/db", null, null, Result.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,16 +1,26 @@
+package client;
+
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    public void clean() {
+        serverFacade.clear();
     }
 
     @AfterAll
