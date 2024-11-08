f2638e3101f62b777ff5e67a8f044906c5984e5d
Bowen Liu <97483739+reouct@users.noreply.github.com>
2024-11-08 14:31:49 -0700

    Update Main.java

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,6 +6,5 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
         new Server().run(8080);
-
     }
 }
